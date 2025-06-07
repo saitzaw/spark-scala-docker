9e4e842504bf6bbfdfb5ad4028fed53fc489c281
saitzaw <saithihazaw43@gmail.com>
2025-06-07 12:53:41 +0700

    update: add more return value to read env

--- notebooks/load_env.py
+++ notebooks/load_env.py
@@ -10,8 +10,16 @@ def env_parameter():
     db_user = os.getenv("POSTGRES_USER")
     db_password = os.getenv("POSTGRES_PASSWORD")
     spark_master = os.getenv("SPARK_MASTER")
+    pg_jar = os.getenv("PG_JAR")
+    event_log_dir = os.getenv("EVENT_LOG_DIR")
+    pg_driver = os.getenv("PG_DIRIVER")
+
     return {
         'jdbc_url': jdbc_url,
         'db_user': db_user, 
         'db_password': db_password, 
-        'spark_master': spark_master}
+        'spark_master': spark_master,
+        'pg_jar': pg_jar,
+        'event_log_dir': event_log_dir,
+        'pg_driver': pg_driver
+        }
