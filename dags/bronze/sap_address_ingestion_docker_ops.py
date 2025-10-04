b5740731b822e2a11e4fa98dd8f31b2980a454d6
saitzaw <saithihazaw43@gmail.com>
2025-10-05 02:23:36 +0700

    fix: remove adrtfrom address

--- dags/bronze/sap_address_ingestion_docker_ops.py
+++ dags/bronze/sap_address_ingestion_docker_ops.py
@@ -1,3 +1,11 @@
+# sap_address_ingestion_docker_ops : version 1.00 Date: 2025-10-04
+#######################################################################
+# Address ingestion using DockerOperator
+#######################################################################
+#    Scheduler process from landing to bronze zone in parquet format.
+#    version: 1.00 Author: Saithiha Zaw Date: 2025-09-28 Create
+########################################################################
+
 from datetime import datetime, timedelta
 import os
 from airflow import DAG
@@ -13,7 +21,7 @@ HOST_LOGS = Variable.get("HOST_SPARK_LOGS")
 IN_APPS = "/opt/spark/jobs"
 IN_LOGS = "/opt/spark/spark-events"
 
-SPARK_IMAGE = "spark-master"   # built from docker/spark.Dockerfile
+SPARK_IMAGE = "spark-image"   # built from docker/spark.Dockerfile
 NETWORK = "enterprise-data-lakehouse_default" 
 
 default_args = {
@@ -22,7 +30,7 @@ default_args = {
     "retry_delay": timedelta(minutes=2),
 }
 
-def spark_task(task_id: str, table: str) -> DockerOperator:
+def spark_task(task_id: str, medallion:str, zone: str, table: str) -> DockerOperator:
     return DockerOperator(
         task_id=task_id,
         image=SPARK_IMAGE,
@@ -32,13 +40,8 @@ def spark_task(task_id: str, table: str) -> DockerOperator:
             "--packages", "org.apache.hadoop:hadoop-aws:3.3.2,com.amazonaws:aws-java-sdk-bundle:1.12.262",
             "--conf", "spark.eventLog.enabled=true",
             "--conf", f"spark.eventLog.dir={IN_LOGS}",
-            # (Optional MinIO config; uncomment if you write to S3/MinIO)
-            # "--conf", "spark.hadoop.fs.s3a.endpoint=http://minio:9000",
-            # "--conf", "spark.hadoop.fs.s3a.path.style.access=true",
-            # "--conf", "spark.hadoop.fs.s3a.access.key=${MINIO_ROOT_USER}",
-            # "--conf", "spark.hadoop.fs.s3a.secret.key=${MINIO_ROOT_PASSWORD}",
-            f"{IN_APPS}/pyspark/src/sap_bronze/{table}.py",
-            # You can append app args here, e.g. "--date_from=2025-09-01"
+            f"{IN_APPS}/pyspark/src/{medallion}/{zone}/{table}.py",
+
         ],
         network_mode=NETWORK,
         auto_remove=True,
@@ -47,9 +50,6 @@ def spark_task(task_id: str, table: str) -> DockerOperator:
             Mount(source=HOST_APPS, target=IN_APPS, type="bind", read_only=True),
             Mount(source=HOST_LOGS, target=IN_LOGS, type="bind"),
         ],
-        # resources (optional):
-        # mem_limit="4g",
-        # cpus=2.0,
     )
 
 with DAG(
@@ -61,9 +61,9 @@ with DAG(
     tags=["spark", "bronze", "sap"],
 ) as dag:
 
-    adr6  = spark_task("adr6_to_bronze",  "adr6")
+    adr6  = spark_task("adr6_to_bronze", "","","adr6")
     adrc  = spark_task("adrc_to_bronze",  "adrc")
     adrct = spark_task("adrct_to_bronze", "adrct")
-    adrt  = spark_task("adrt_to_bronze",  "adrt")
 
-    adr6 >> adrc >> adrct >> adrt
+
+    adr6 >> adrc >> adrct 
