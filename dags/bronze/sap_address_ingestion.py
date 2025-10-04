0007920539857a5d7b9ed8b7521199f766b48c15
saitzaw <saithihazaw43@gmail.com>
2025-10-05 02:24:45 +0700

    fix: remove adrt

--- dags/bronze/sap_address_ingestion.py
+++ dags/bronze/sap_address_ingestion.py
@@ -1,3 +1,12 @@
+# sap_address_ingestion : version 1.01 Date: 2025-10-04
+#######################################################################
+# Scheduler Address ingestion using BashOperator
+#######################################################################
+#    Scheduler process from landing to bronze zone in parquet format.
+#    version: 1.00 Author: Saithiha Zaw Date: 2025-09-28 Create
+#    version: 1.01 Author: Saithiha Zaw Date: 2025-10-04 Modify to use BashOperator
+#######################################################################
+
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
@@ -14,27 +23,22 @@ with DAG(
     schedule_interval=None,
     catchup=False,
     default_args=default_args,
-    tags=["spark", "bronze", "sap"],
+    tags=["spark", "bronze", "sap"]
 ) as dag:
 
     adr6_task = BashOperator(
         task_id="adr6_to_bronze",
-        bash_command="/opt/airflow/dags/scripts/spark_submit.sh adr6 ",
+        bash_command='bash -lc "/opt/airflow/dags/scripts/spark_submit.sh sap_bronze address adr6" ',
     )
 
     adrc_task = BashOperator(
         task_id="adrc_to_bronze",
-        bash_command="/opt/airflow/dags/scripts/spark_submit.sh adrc ",
+        bash_command='bash -lc "/opt/airflow/dags/scripts/spark_submit.sh sap_bronze address adrc" ',
     )
 
     adrct_task = BashOperator(
         task_id="adrct_to_bronze",
-        bash_command="/opt/airflow/dags/scripts/spark_submit.sh adrct ",
+        bash_command='bash -lc "/opt/airflow/dags/scripts/spark_submit.sh sap_bronze address adrct" ',
     )
 
-    adrt_task = BashOperator(
-        task_id="adrt_to_bronze",
-        bash_command="/opt/airflow/dags/scripts/spark_submit.sh adrt ",
-    )
-
-    adr6_task >> adrc_task >> adrct_task >> adrt_task
+    adr6_task >> adrc_task >> adrct_task 
